30a002a2cbd446ef28a8a3e6e77d8bfff157c743
SeongYong <pjii50260@naver.com>
2022-09-28 16:23:49 +0900

    Create 3_1.cpp

---
+++ Chapter3/3_1.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+class Circle{
+    public:
+        int radius;
+        double getArea();
+};
+
+double Circle::getArea(){
+    return 3.14*radius*radius;
+}
+
+int main(){
+    Circle donut;
+    donut.radius = 1;
+    double area = donut.getArea();
+    cout << "the area of donut is" << area << endl;
+
+    Circle pizza;
+    pizza.radius = 30;
+    area = pizza.getArea();
+    cout << "the area of pizza is" << area << endl;
+}
+
