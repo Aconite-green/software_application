5e2bf670c6e919936006e6c3caf768652eeb2a59
SeongYong <pjii50260@naver.com>
2022-11-11 15:13:34 +0900

    Update ex6.cpp

--- Chapter8/ex6.cpp
+++ Chapter8/ex6.cpp
@@ -1,37 +1,52 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-class BaseArray{
+class BaseArray {
 private:
 	int capacity;
-	int *mem;
+	int* mem;
 protected:
-	BaseArray(int capacity=100){
-		this->capacity = capacity; mem = new int [capacity];
+	BaseArray(int capacity = 100) {
+		this->capacity = capacity; mem = new int[capacity];
 	}
-	~BaseArray() { delete [] mem;}
-	void put(int index, int val){mem[index] = val;}
-	int get(int index){return mem[index];}
-	int getCapacity(){return capacity;}
-}; 
-
-class MyStack:public BaseArray{
-
+	~BaseArray() { delete[] mem; }
+	void put(int index, int val) { mem[index] = val; }
+	int get(int index) { return mem[index]; }
+	int getCapacity() { return capacity; }
 };
 
-int main(){
+class MyStack:public BaseArray{
+	int index=0;
+public:
+	MyStack(int capacity):BaseArray(capacity) {
+	}
+	void push(int n) {
+		put(index, n);
+		index++;
+}
+	int capacity() {
+		return getCapacity();
+	}
+	int length() {
+		return index;
+	}
+	int pop() {
+		return get(--index);
+	}
+};
+int main() {
 	MyStack mStack(100);
 	int n;
-	cout << "put 5 integer>> ";
-	for(int i=0;i<5;i++){
+	cout << "스택에 삽입할 5개의 정수를 입력하라>> ";
+	for (int i = 0; i < 5; i++)
+	{
 		cin >> n;
 		mStack.push(n);
 	}
-
-	cout << "stack capacity: " << mStack.capacity() << ", stack size: " << mStack.length() << endl;
-	cout << "pop all the element in the stack>> ";
-	while(mStack.length() != 0){
+	cout << "스택용량:" << mStack.capacity() << ", 스택크기:" << mStack.length() << endl;
+	cout << "스택의 모든 원소를 팝하여 출력한다>> ";
+	while (mStack.length() != 0) {
 		cout << mStack.pop() << ' ';
 	}
-	cout << endl << "the size of current stack : " << mStack.length() << endl;
+	cout << endl << "스택의 현재 크기: " << mStack.length() << endl;
 }
