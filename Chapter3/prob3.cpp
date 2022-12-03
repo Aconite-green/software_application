eec0b0b9ef51396e2f5103576bb515f7a557261c
SeongYong <pjii50260@naver.com>
2022-12-03 11:33:47 +0900

    commit

--- Chapter3/prob3.cpp
+++ Chapter3/prob3.cpp
@@ -14,12 +14,23 @@ class Random{
         int next();
         int nextInRange(int start, int end);
 };
-int Random::next(){
 
+Random::Random(){}
+
+int Random::next(){
+    int n = rand();
+    return n;
+}
+int Random::nextInRange(int min, int max){
+    int n = rand();
+    n = n % (max - min + 1) + min;
+    return n;
 }
 
 //문제 부분
 int main(){
+    srand((unsigned)time(0));
+    
     Random r;
     cout << "-- 0에서 " << RAND_MAX << "까지의 랜덤 정수 10 개" << "\n";
     for(int i=0; i<10; i++){
